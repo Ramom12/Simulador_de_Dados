c1f26b60d6fb1e87711e01675c7412690fbe47ff
Ramom <ramom.santos@aluno.fapce.edu.br>
2021-12-21 21:41:53 -0500

    melhorando o código

--- simulador_de_dados.py
+++ simulador_de_dados.py
@@ -1,28 +1,31 @@
-# Simular um dado indo de 1 ao número 6
 import random
-
+import PySimpleGUI as sg
 
 class SimuladorDeDado:
     def __init__(self):
         self.valor_inicial = 1
         self.valor_final = 6
-        self.mensagem = 'Você gostaria de jogar o dado??'
-
-    def iniciar(self):
-        resposta = input(self.mensagem)
+        # Layout
+        self.layout = [
+            [sg.Text('Você gostaria de jogar o dado?')],
+            [sg.Button('sim'),sg.Button('Não')]
+        ]
+    
+    def Iniciar(self):
+        self.janela = sg.Window('Simulador de Dado',layout=self.layout)
+        self.eventos, self.valores = self.janela.Read()
         try:
-            if resposta == 'sim' or resposta == 's':
+            if self.eventos == 'sim' or self.eventos == 's':
                 self.GerarValorDoDado()
-            elif resposta == 'não' or resposta == 'n':
-                print('Obrigado pela sua participação')
+            elif self.eventos == 'Não' or self.eventos == 'n':
+                print('Obrigado por participar!')
             else:
-                print('Favor Digitar sim ou não')
+                print('Favor digitar sim ou não')
         except:
             print('Ocorreu um erro ao receber sua resposta')
-
+    
     def GerarValorDoDado(self):
-        print(random.randint(self.valor_inicial, self.valor_final))
-
+        print(random.randint(self.valor_inicial,self.valor_final))
 
 simulador = SimuladorDeDado()
-simulador.iniciar()
+simulador.Iniciar()
